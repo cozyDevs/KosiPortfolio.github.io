38024a3b3c852198a755e9e6e69944a604101886
Kosi Chima <nanomyte1@gmail.com>
2023-08-07 23:17:15 -0400

    Finished the Edits to the games Page , final comit

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
-const modal = document.querySelector("#modal");
+const modalSeedling = document.querySelector("#modalSeedling");
 const modalGrave = document.querySelector("#modalGrave");
+const modalFOD = document.querySelector("#modalFOD");
 
-const openModal = document.querySelector(".open-button");
-const closeModal = document.querySelector(".close-button");
+
+const openModalSeedling = document.querySelector(".open-button-Seedling");
+const closeModalSeedling = document.querySelector(".close-button-Seedling");
 const openModalGrave = document.querySelector(".open-button-grave");
 const closeModalGrave = document.querySelector(".close-button-grave");
+const openModalFOD = document.querySelector(".open-button-FOD");
+const closeModalFOD = document.querySelector(".close-button-FOD");
 
-openModal.addEventListener("click", () => {
-  modal.showModal();
-  modal.scrollTop = 0;
+openModalSeedling.addEventListener("click", () => {
+  modalSeedling.showModal();
+  modalSeedling.scrollTop = 0;
+  modalSeedling.classList.remove("hide-modal");
+  modalSeedling.classList.add("show-modal");
+  
  
 });
 
-closeModal.addEventListener("click", () => {
-  modal.close();
+closeModalSeedling.addEventListener("click", () => {
+  modalSeedling.close();
+  modalSeedling.classList.remove("show-modal");
+  modalSeedling.classList.add("hide-modal");
 
 });
 
 openModalGrave.addEventListener("click", () => {
   modalGrave.showModal();
   modalGrave.scrollTop =0;
+  modalGrave.classList.remove("hide-modal");
+  modalGrave.classList.add("show-modal");
 })
 
 closeModalGrave.addEventListener("click", () => {
  modalGrave.close();
+ modalGrave.classList.remove("show-modal");
+  modalGrave.classList.add("hide-modal");
+})
+openModalFOD.addEventListener("click", () => {
+  modalFOD.showModal();
+  modalFOD.scrollTop =0;
+  modalFOD.classList.remove("hide-modal");
+  modalFOD.classList.add("show-modal");
+})
+
+closeModalFOD.addEventListener("click", () => {
+ modalFOD.close();
+ modalFOD.classList.remove("show-modal");
+  modalFOD.classList.add("hide-modal");
 })
